7ba54d83216e5eff6b66775af9099ee1c13a359c
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-26 12:28:17 +0530

    Add arguments to detail view navigation | Make Marsproperty parcelable

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,13 +17,16 @@
 
 package com.example.android.marsrealestate.network
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 class MarsProperty(
         val id: String,
         @Json(name = "img_src") val imgSrc: String,
         val price: Double,
-        private val type: String) {
+        private val type: String) : Parcelable {
     val isRental
         get() = type == "rent"
 }
