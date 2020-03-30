8b9adb405702269e0162582cde96f93ad6f20fde
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-31 02:10:46 +0530

    Create refresh data worker

---
+++ app/src/main/java/com/example/android/marsrealestate/data/work/RefreshDBWorker.kt
@@ -0,0 +1,21 @@
+package com.example.android.marsrealestate.data.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.marsrealestate.data.database.PropertyDatabase
+import com.example.android.marsrealestate.repository.PropertyRepository
+import retrofit2.HttpException
+
+class RefreshDBWorker(private val context: Context, params: WorkerParameters) : CoroutineWorker(context, params) {
+    override suspend fun doWork(): Result {
+        val db = PropertyDatabase.getInstance(context)
+        val repository = PropertyRepository(db)
+        try {
+            repository.refreshDatabase()
+        } catch (e: HttpException) {
+            return Result.retry()
+        }
+        return Result.success()
+    }
+}
