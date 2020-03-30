8f1a4d3875603effec3a51a6b4bff5679e8d826c
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-30 20:57:49 +0530

    Create property repository

--- app/src/main/java/com/example/android/marsrealestate/repository/PropertyRepository.kt
+++ app/src/main/java/com/example/android/marsrealestate/repository/PropertyRepository.kt
@@ -1,4 +1,19 @@
 package com.example.android.marsrealestate.repository
 
-class PropertyRepository {
+import com.example.android.marsrealestate.data.database.PropertyDatabase
+import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsApiFilter
+import com.example.android.marsrealestate.network.toDbModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class PropertyRepository(private val propertyDatabase: PropertyDatabase) {
+
+    suspend fun refreshDatabase() {
+        withContext(Dispatchers.IO) {
+            //remove this filter thing afterwards
+            val propertyListFromNetwork = MarsApi.retrofitService.getPropertiesAsync(MarsApiFilter.SHOW_ALL.value).await()
+            propertyDatabase.dao.insertAll(propertyListFromNetwork.toDbModel())
+        }
+    }
 }
