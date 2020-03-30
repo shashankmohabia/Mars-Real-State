02e9043c8633b1ed46555d334422319de37d1fd5
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-30 21:08:38 +0530

    Add properties list to get data from db to be used in views

--- app/src/main/java/com/example/android/marsrealestate/repository/PropertyRepository.kt
+++ app/src/main/java/com/example/android/marsrealestate/repository/PropertyRepository.kt
@@ -1,6 +1,9 @@
 package com.example.android.marsrealestate.repository
 
+import android.view.animation.Transformation
+import androidx.lifecycle.Transformations
 import com.example.android.marsrealestate.data.database.PropertyDatabase
+import com.example.android.marsrealestate.data.models.toDomainModel
 import com.example.android.marsrealestate.network.MarsApi
 import com.example.android.marsrealestate.network.MarsApiFilter
 import com.example.android.marsrealestate.network.toDbModel
@@ -8,6 +11,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class PropertyRepository(private val propertyDatabase: PropertyDatabase) {
+
+    val properties = Transformations.map(propertyDatabase.dao.getAllProperties()) {
+        it.toDomainModel()
+    }
 
     suspend fun refreshDatabase() {
         withContext(Dispatchers.IO) {
